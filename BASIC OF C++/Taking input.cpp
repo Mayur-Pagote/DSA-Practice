5ae09e3e925d84d46a415b3a4606e446e62c1269
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-19 09:06:31 +0530

    Create Taking input.cpp

---
+++ BASIC OF C++/Taking input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    cout<<endl;
+    cout<<"Number is: "<<num;
+    return 0;
+}
