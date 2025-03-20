2352833154fb4e66a47f8f636e407336f0cc295d
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-20 17:54:07 +0530

    Create Take an integer between 65 and 90 as input and print its corresponding uppercase letter.cpp

---
+++ Assignment 1/Take an integer between 65 and 90 as input and print its corresponding uppercase letter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int char_num;
+    cout<<"Enter a number between 65 to 90: ";
+    cin>>char_num;
+    cout<<endl;
+    
+    cout<<"Corresponding uppercase letter is: "<<(char)char_num;
+    
+    return 0;
+    
+}
