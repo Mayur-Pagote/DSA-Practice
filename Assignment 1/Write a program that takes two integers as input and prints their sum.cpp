bcfc53f22737eedd3e24d26b95a7e325cdb2ca49
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-20 17:37:29 +0530

    Create Write a program that takes two integers as input and prints their sum.cpp

---
+++ Assignment 1/Write a program that takes two integers as input and prints their sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num1, num2;
+    cout<<"Enter first number: ";
+    cin>>num1;
+    cout<<endl;
+    cout<<"Enter second number: ";
+    cin>>num2;
+    cout<<endl;
+    cout<<"Sum is: "<<num1+num2;
+    
+    return 0;
+    
+}
