4e138dfd0184ec834fd2cd02d3d7aa8bb1b43752
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-20 18:09:08 +0530

    Create Question 13.cpp

---
+++ Assignment 1/Question 13.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main() 
+{
+    int x1, x2, y1, y2, dist;
+    cout<<"Enter x1: ";
+    cin>>x1;
+    cout<<endl;
+    cout<<"Enter x2: ";
+    cin>>x2;
+    cout<<endl;
+    cout<<"Enter y1: ";
+    cin>>y1;
+    cout<<endl;
+    cout<<"Enter y2: ";
+    cin>>y2;
+    cout<<endl;
+    
+    dist = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
+    
+    cout<<"Eucledian Distance is: "<<sqrt(dist);
+    
+}
