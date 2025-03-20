f3b9ebc288111abbe6d3b1193f858ee9667ce3e5
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-20 17:42:44 +0530

    Create Take two numbers as input and perform addition, subtraction, multiplication and division.cpp

---
+++ Assignment 1/Take two numbers as input and perform addition, subtraction, multiplication and division.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num1, num2;
+    cout<<"Enter first number: ";
+    cin>>num1;
+    cout<<endl;
+    cout<<"Enter second number: ";
+    cin>>num2;
+    cout<<endl;
+    cout<<"Sum is: "<<num1+num2<<endl;
+    cout<<"Difference is: "<<num1-num2<<endl;
+    cout<<"Product is: "<<num1*num2<<endl;
+    cout<<"Division is: "<<num1/num2<<endl;
+    
+    return 0;
+    
+}
