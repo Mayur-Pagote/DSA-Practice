e09c800c9d01eac00c9caf03079fda456cd277d6
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-20 17:48:07 +0530

    Create Take a character as input and print its corresponding ASCII value.cpp

---
+++ Assignment 1/Take a character as input and print its corresponding ASCII value.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    char s;
+    cout<<"Enter a character: ";
+    cin>>s;
+    cout<<endl;
+    
+    cout<<"ASCII value is "<<(int) s;
+    
+    
+    return 0;
+    
+}
