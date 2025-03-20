14596de14e42bf3c1e957be0731ea858fdae8054
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-20 17:45:32 +0530

    Create Take input of principle, time and rate and find the simple interest.cpp

---
+++ Assignment 1/Take input of principle, time and rate and find the simple interest.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    double p, r, t, si;
+    cout<<"Enter principle: ";
+    cin>>p;
+    cout<<endl;
+    cout<<"Enter rate: ";
+    cin>>r;
+    cout<<endl;
+    cout<<"Enter time: ";
+    cin>>t;
+    cout<<endl;
+    
+    si = (p*r*t)/100;
+    
+    cout<<"Simple interest is: "<<si;
+    
+    
+    return 0;
+    
+}
