c03bc7d736a17c7937bde177be7efdd1d41990de
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-20 17:50:56 +0530

    Create Take a floating point number as input typecast it into an integer and print both values.cpp

---
+++ Assignment 1/Take a floating point number as input typecast it into an integer and print both values.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    float num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    cout<<endl;
+    
+    cout<<"Float number is "<<num<<endl;
+    cout<<"Int conversion is "<<(int) num;
+    
+    
+    return 0;
+    
+}
