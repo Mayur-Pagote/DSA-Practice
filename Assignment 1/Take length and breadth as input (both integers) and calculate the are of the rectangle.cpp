ae68282f22e2a06c769fc3421538ddea912a61e2
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-20 17:40:41 +0530

    Create Take length and breadth as input (both integers) and calculate the are of the rectangle.cpp

---
+++ Assignment 1/Take length and breadth as input (both integers) and calculate the are of the rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    double length, breadth;
+    cout<<"Enter the length: ";
+    cin>>length;
+    cout<<endl;
+    cout<<"Enter the breadth: ";
+    cin>>breadth;
+    cout<<endl;
+    cout<<"Area is: "<<length * breadth;
+    
+    return 0;
+    
+}
