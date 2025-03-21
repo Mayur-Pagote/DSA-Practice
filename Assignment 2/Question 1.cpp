67426aac951419d68d570445f7444591151bd592
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-21 16:51:34 +0530

    Create Question 1.cpp

---
+++ Assignment 2/Question 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num1, num2;
+    cout<<"Enter first number: ";
+    cin>>num1;
+    cout<<endl;
+    
+    cout<<"Enter second number: ";
+    cin>>num2;
+    cout<<endl;
+    
+    if (num1 > num2){
+      cout<<"First number is greater";
+    }
+    else cout<<"Second number is greater";
+}
