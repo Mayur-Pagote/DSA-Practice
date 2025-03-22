17b58da39a20ad3054465205619e199d211627eb
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-22 16:49:38 +0530

    Update Question 14.cpp

--- Assignment 1/Question 14.cpp
+++ Assignment 1/Question 14.cpp
@@ -1,3 +1,9 @@
+//Write a program that will tell the number of dogs and chicken are there when the
+//user will provide the value of total heads and legs.
+//For example:
+//Input: heads -> 4 legs -> 12
+//Output: dogs -> 2 chicken -> 2
+
 #include <iostream>
 #include <math.h>
 using namespace std;
