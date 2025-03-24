955dd62c1ab4cf17fad71e7a4a634b7d92a3e9ee
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-24 17:43:45 +0530

    Create Question 2.cpp

---
+++ Assignment 7/Question 2.cpp
@@ -0,0 +1,23 @@
+// Write a function that takes the radius of a circle as an argument and returns
+// its area.
+
+#include <iostream>
+using namespace std;
+
+void find_circle_area(float r){
+    float pi =  3.14;
+    cout<<"Area of circle is: "<<pi * r * r;
+}
+
+
+int main() 
+{
+    int radius;
+    cout<<"Enter radius: ";
+    cin>>radius;
+    cout<<endl;
+    
+    find_circle_area(radius);
+    
+    
+}
