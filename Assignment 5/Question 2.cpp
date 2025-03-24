816eee6a7ff8c0a1b94f21156c444fe57ab1cbce
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-24 08:57:53 +0530

    Create Question 2.cpp

---
+++ Assignment 5/Question 2.cpp
@@ -0,0 +1,20 @@
+// Print the following pattern
+// Input: n = 4
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    cout<<endl;
+    
+    for (int i = n; i>=1; i--){
+      for (int j = 1; j<=i; j++){
+        cout<<j<<" ";
+      }
+      cout<<endl;
+    }
+}
