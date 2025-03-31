24b4a80188b148dce538b3b7df009c480194b9ff
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-03-31 15:58:45 +0530

    Create Question 1.cpp

---
+++ Assignment 6/Question 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n, count = 1;;
+    cout<<"Enter a number: ";
+    cin>>n;
+    cout<<endl;
+    
+    for(int i = n; i>=1; i--){
+      for (int j = i; j>1; j--){
+        cout<<" ";
+      }
+      for (int k = 1; k <= count; k++){
+        cout<<k;
+      }
+      cout<<endl;
+      count += 2;
+    }
+}
